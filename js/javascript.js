785e21f3db821752abbc2a488018f9b010b75c9a
eSSeGH <ste.virgadaula@gmail.com>
2023-02-01 16:16:38 +0100

    EDIT html ids + JS suspended for TICKET

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+// inserire in un input il numero di km da percorrere
+
+// selezionare da una select il tipo di sconto da applicare 
+// (3 scelte: sconto minorenne, sconto over 65, nessuno sconto)
+
+// Al click sul pulsante calcolare il prezzo del biglietto:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
