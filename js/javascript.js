2ca1ebb26def4d077d26643ebd99447f8c7de8e5
eSSeGH <ste.virgadaula@gmail.com>
2023-02-01 16:17:29 +0100

    EDIT html ids + JS suspended for TICKET

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,14 @@
-// inserire in un input il numero di km da percorrere
+// inserire in un input nome e il numero di km da percorrere
+
+const passengerName = document.getElementById("passenger-name")
+
+const km = document.getElementById("km")
+console.log(passengerName, km)
 
 // selezionare da una select il tipo di sconto da applicare 
 // (3 scelte: sconto minorenne, sconto over 65, nessuno sconto)
+
+let underage = document.querySelector("#offer option[value="underage"]").value
 
 // Al click sul pulsante calcolare il prezzo del biglietto:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
